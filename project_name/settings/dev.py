9db452a4dbe513cf931693ad1b525de274719368
James <james@ubuntu.ubuntu-domain>
2013-07-25 18:33:54 +0100

    project_name/settings/base.py
    modfied setting, debug toolbar

--- project_name/settings/dev.py
+++ project_name/settings/dev.py
@@ -11,10 +11,21 @@ EMAIL_PORT = 1025
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', 
-		'NAME': os.path.join(VAR_ROOT, 'dev.db'),    
+		'NAME': 'dev.db',    
 		}
 }
 
 INSTALLED_APPS += ("debug_toolbar",) 
 INTERNAL_IPS = ("127.0.0.1")
-MIDDLEWARE_CLASSES += ("debug_toolbar.middleware.DebugToolbarMiddleware")
+MIDDLEWARE_CLASSES += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
+
+def custom_show_toolbar(request):
+    return True  # Always show toolbar, for example purposes only.
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar,
+    'HIDE_DJANGO_SQL': False,
+    'TAG': 'div',
+    'ENABLE_STACKTRACES' : True,
+}
